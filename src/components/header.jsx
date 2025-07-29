ba90ffea2faa7f4943d59a6c23b30351a8147a16
TurnerHelical <hunterde91@gmail.com>
2025-07-29 09:42:39 -0400

    added basic header and a slide-in element, styled header slightly

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,13 @@
 import "../styles/header.css";
 
 function Header() {
+    window.onload = () => {
+        const title = document.querySelector('.title');
+        title.classList.add('slide-in');
+    }
   return <>
             <header>
-                <h1>Resume Generator</h1>
+                <h1 className="title">Resume Generator</h1>
             </header>
   
         </>;
